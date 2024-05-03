5b1022bfa630242cfb04a364de615dbdc72b8d54
gmangan-smartling <43435938+gmangan-smartling@users.noreply.github.com>
2024-05-03 13:30:54 +0100

    Update dcs_comp.js

--- dcs_comp.js
+++ dcs_comp.js
@@ -1,4 +1,4 @@
-txt='{"response":{"comp_div": "Div text with slCompositeString attribute", "inside_comp": " text for nested p tag in div with slCompositeString.", "div_no_comp": "This div has no attribute", "p_no_comp": " and p tag is nested in a no comp div.", "div_create_node": "This div is created differently with a nested p tag created in JS ", "p_node_creation": "p tag from javascript only."}}'
+txt='{"response":{"comp_div": "Div text with slCompositeString attribute", "inside_comp": " text for nested p tag in div with slCompositeString.", "div_no_comp": "This div has no attribute", "p_no_comp": " and p tag is nested in a no comp div.", "div_create_node": "This div is created differently with a nested p tag created in JS ", "p_node_creation": "p tag from javascript only.", "pro_p":"P tag element with a ", "pro_span": "span text", "pro_link": "link"}}'
 console.log(txt)
 var myObj = JSON.parse(txt);
 function compDiv(){
@@ -10,6 +10,7 @@ document.getElementById('no_comp_div').innerHTML = myObj.response.div_no_comp + 
 function createNodeJS(){
 document.getElementById('div_create_node').innerHTML = myObj.response.div_create_node
 const para = document.createElement("p");
+para.setAttribute("slCompositeString", "true")
 
 // Create a text node:
 const node = document.createTextNode(myObj.response.p_node_creation);
@@ -19,4 +20,18 @@ para.appendChild(node);
 
 // Append the p element to the body:
 document.getElementById("div_create_node").appendChild(para);
+}
+function progressive(){
+document.getElementById('progressive').innerHTML = myObj.response.pro_p
+const span = document.createElement("span");
+span.setAttribute("slCompositeString", "true")
+
+// Create a text node:
+const span_node = document.createTextNode(myObj.response.pro_span);
+
+// Append text node to the p element:
+span.appendChild(span_node);
+
+// Append the p element to the body:
+document.getElementById("progressive").appendChild(span);
 }
